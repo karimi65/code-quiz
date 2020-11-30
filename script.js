c90419a84866cf0d5547d250f662e3df6931803f
Captain <71038018+karimi65@users.noreply.github.com>
2020-11-30 10:24:55 -0600

    add var questions

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+var questins = [
+    {
+        question1: 'Arrays in Javascript can be used to store ____.',
+        answers: ['numbers and strings', 'booleans', 'other arrays', 'all of the above'],
+        correct: 'all of the above' 
+    },
+    {
+        question2: 'Which computer language structures all webpages on the internet?',
+        answers: ['HTML', 'Javascript', 'Java', 'CSS'],
+        correct: 'HTML',
+    },
+    {
+        questin3: 'What tag do you need to use to enclose the Javascript code?',
+        answers: ['body', 'script', 'style', 'code'],
+        correct: 'script',
+    },
+    {
+        question4: 'Which one of these HTML tags creates a button?',
+        answers: ['<h1>', '<video>', '<button>', '<p>'],
+        correct: '<button>',
+    },
+    {
+        questin5: 'Where should the style tag be declared to organize an internal CSS?',
+        answers: ['body', 'external file', 'head', 'both 2 and 3'],
+        correct: 'head'
+    },
+
+];
+
 var startButton = document.getElementById('start');
  
 startButton.addEventListener("click", startQuiz);
+
+
  
 function startQuiz() {
     var myTime = 60;
@@ -14,3 +45,8 @@ function startQuiz() {
         }
     }, 1000)
 }
+
+function codeQuestion() {
+
+}
+
