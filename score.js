657eddc5abcaacdda85fc10002d6ba8d190e5ee5
Captain <71038018+karimi65@users.noreply.github.com>
2020-12-02 23:44:02 -0600

    addEventListener for clear scores

--- score.js
+++ score.js
@@ -2,3 +2,10 @@ var scores = document.getElementById('scores');
 var clearBtn = document.querySelector('#clear');
 var backBtn = document.querySelector('#back');
 
+// clear scores
+clearBtn.addEventListener('click', clearScores);
+
+function clearScores() {
+    localStorage.clear();
+    location.reload();
+};
