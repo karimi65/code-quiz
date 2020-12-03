6d21795b741b368d73ef5f9b5f74b1cf012fadf9
Captain <71038018+karimi65@users.noreply.github.com>
2020-12-03 00:16:39 -0600

    get data from local storage

--- score.js
+++ score.js
@@ -9,3 +9,21 @@ function clearScores() {
     localStorage.clear();
     location.reload();
 };
+// get data from local storage
+var allUserScores = localStorage.getItem("allUserScores");
+allUserScores = Json.parse(allUserScores);
+
+if (allUserScores !== null) {
+
+    for (var i = 0; i < allUserScores.length; i++) {
+        var liTag = document.createElement('li');
+        liTag.textContent = allUserScores[i].initials + " " + allUserScores[i].finalScore;
+        scores.appendChild(litag);
+    }
+}
+
+backBtn.addEventListener('click', goBack);
+
+function goBack() {
+    window.location.reload("index.html");
+}
